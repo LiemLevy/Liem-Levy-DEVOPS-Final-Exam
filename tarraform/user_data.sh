904aaf041f3dcfc89cd5a5ac770ee5ed11e7a608
Liem Levy <liem36194@gmail.com>
2025-11-05 18:22:10 +0200

    Create user_data.sh

---
+++ tarraform/user_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Update system
+yum update -y
+
+# Install Docker
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+
+# Add ec2-user to docker group
+usermod -aG docker ec2-user
+
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install Git
+yum install -y git
+
+# Verify installations
+docker --version
+docker-compose --version
+git --version
